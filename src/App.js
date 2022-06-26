2877bc5eb24c3fda9d38afc0292ef4665b7700a7
박서정 <12211197@inha.edu>
2022-06-26 20:30:34 +0900

    리액트 5주차 과제

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import WelcomePage from "./pages/WelcomePage";
+import { Routes, Route } from "react-router-dom";
+import Article from "./pages/Article";
+import CreateArticle from "./pages/CreateArticle";
+import EditArticle from "./pages/EditArticle";
+import NotFound from "./pages/NotFound";
+import Owner from "./pages/Owner";
+
+const API_URL = "https://guestbook.devhudi.xyz";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<div>Home Page</div>} />
-        <Route path="/hello" element={<>Hello, World!</>} />
-        <Route path="/welcome/:name" element={<WelcomePage />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/:ownerId" element={<Owner apiUrl={API_URL} />} />
+      <Route
+        path="/:ownerId/create"
+        element={<CreateArticle apiUrl={API_URL} />}
+      />
+      <Route
+        path="/:ownerId/:articleId"
+        element={<Article apiUrl={API_URL} />}
+      />
+      <Route
+        path="/:ownerId/:articleId/edit"
+        element={<EditArticle apiUrl={API_URL} />}
+      />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 };
 
